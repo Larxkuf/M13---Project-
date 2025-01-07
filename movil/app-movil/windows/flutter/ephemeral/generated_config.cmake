f4bb97785105c3611cae652ea0c6ff309c02283a
Laura <153616329+Larxkuf@users.noreply.github.com>
2025-01-07 19:44:54 +0100

    Add files via upload

---
+++ movil/app-movil/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Programas\\xamp\\htdocs\\app-movil" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=D:\\Programas\\xamp\\htdocs\\app-movil"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Programas\\xamp\\htdocs\\app-movil\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Programas\\xamp\\htdocs\\app-movil"
+  "FLUTTER_TARGET=D:\\Programas\\xamp\\htdocs\\app-movil\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Programas\\xamp\\htdocs\\app-movil\\.dart_tool\\package_config.json"
+)
